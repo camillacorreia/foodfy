2134717fed34863447644b4eabbc7c27ee51a386
Camilla Correia <50275354+camillacorreia@users.noreply.github.com>
2020-07-18 00:11:53 -0300

    Criação da admin

--- controllers/RecipesController.js
+++ controllers/RecipesController.js
@@ -1,2 +1,38 @@
 const fs = require('fs');
 const data = require('../data.json');
+
+// index
+exports.index = function(req, res) {
+    return res.render("admin/index", { recipes: data.recipes });
+};
+
+// create
+exports.create = function (req, res) {
+    return res.render("admin/create");
+};
+
+// show
+exports.show = function (req, res) {
+    const { id } = req.params;
+
+    const foundRecipe = data.recipes.find(function(recipe) {
+      return id == recipe.id;
+    });
+
+    if (!foundRecipe) return res.send("Receita não encontrada!");
+
+    return res.render("admin/show", { recipe: foundRecipe });
+}
+
+//edit
+exports.edit = function (req, res) {
+    const { id } = req.params;
+
+    const foundRecipe = data.recipes.find(function(recipe) {
+      return id == recipe.id;
+    });
+
+    if (!foundRecipe) return res.send("Receita não encontrada!");
+
+    return res.render("admin/edit", { recipe: foundRecipe });
+};
--- routes.js
+++ routes.js
@@ -5,10 +5,16 @@ const home = require('./controllers/HomeController');
 const about = require('./controllers/AboutController');
 const recipeshome = require('./controllers/RecipesHomeController');
 const details = require('./controllers/DetailsController');
+const recipes = require('./controllers/RecipesController');
 
 routes.get("/", home.index); // Página Principal
 routes.get("/about", about.index); // Sobre
 routes.get("/recipes", recipeshome.index); // Listagem de receitas
 routes.get("/recipes/:id", details.index); // Detalhes da receita
+
+routes.get("/admin/recipes", recipes.index); // Mostrar a lista de receitas
+routes.get("/admin/recipes/:id", recipes.show); // Exibir detalhes de uma receita
+routes.get("/admin/recipes/create", recipes.create); // Mostrar formulário de nova receita
+routes.get("/admin/recipes/:id/edit", recipes.edit); // Mostrar formulário de edição de receita
 
 module.exports = routes;
